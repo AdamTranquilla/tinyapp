2b842644acacf45ef17ab452a52e784853313548
Adam Tranquilla <adam.tranquilla@gmail.com>
2020-12-07 22:26:01 +0000

    url shortener started

--- express_server.js
+++ express_server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+function generateRandomString() {
+  Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+}
+console.log(generateRandomString())
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -24,6 +33,15 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase}; // must be an object. this is so it can be accessed by key
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
